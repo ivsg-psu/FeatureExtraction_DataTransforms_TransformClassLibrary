c14802a082c8eca4a20ca0281c1ee7ce181d04a7
Mariam Abdellatief <97060004+Mariam-Abdellatief@users.noreply.github.com>
2023-07-06 15:26:25 -0400

    Update script_test_fcn_transform_encoderCounts.m

--- Functions/script_test_fcn_transform_encoderCounts.m
+++ Functions/script_test_fcn_transform_encoderCounts.m
@@ -11,15 +11,15 @@ close all
 
 %% Assertions / Testing outputs
 wheel_radius = 0.3;
-initial_counts_rear_left = 9;
-initial_counts_rear_right = 11;
-counts_per_revolution = 2;
+initial_counts_rear_left = 0;
+initial_counts_rear_right = 0;
+counts_per_revolution = 5;
 
 %% Case: Two input velocities
 wheel_velocity_rear_left = [11,2];
 wheel_velocity_rear_right = [9,7];
 delta_time = 5;
-[discrete_encoder_count_rear_left, discrete_encoder_count_rear_right] = fcn_transform_encoderCounts(wheel_velocity_rear_left,wheel_velocity_rear_right,wheel_radius,initial_counts_rear_left,initial_counts_rear_right,delta_time,counts_per_revolution); %#ok<ASGLU>
+[discrete_encoder_count_rear_left, discrete_encoder_count_rear_right] = fcn_transform_encoderCounts(wheel_velocity_rear_left,wheel_velocity_rear_right,wheel_radius,initial_counts_rear_left,initial_counts_rear_right,delta_time,counts_per_revolution); 
 expected_values.encoder_rear_left = [2312,2730]; %-->>IMP QUESTION: assertion fails bec the calculation takes the
 % previous encoder value without floor, but by hand I used floored value
 % --> Which is correct??
@@ -46,6 +46,42 @@ hold on
 plot(t,discrete_encoder_count_rear_left)
 hold off
 legend("Sine Wave Velocities", "Rear Left Encoder Counts")
+
+%% Case: Increasing then decreasing velocity 
+wheel_velocity_rear_left = [1,2,3,4,5,6,5,4,3,2,1];
+wheel_velocity_rear_right = [1,2,3,4,5,6,5,4,3,2,1];
+delta_time = 1;
+t = [0,1,2,3,4,5,6,7,8,9,10];
+[discrete_encoder_count_rear_left, discrete_encoder_count_rear_right] = fcn_transform_encoderCounts(wheel_velocity_rear_left,wheel_velocity_rear_right,wheel_radius,initial_counts_rear_left,initial_counts_rear_right,delta_time,counts_per_revolution);
+plot(t,wheel_velocity_rear_left)
+hold on
+plot(t,discrete_encoder_count_rear_left)
+hold off
+legend("Velocities", "Rear Left Encoder Counts")
+
+%% Case: Decreasing velocity 
+wheel_velocity_rear_left = [6,5,4,3,2,1];
+wheel_velocity_rear_right = [6,5,4,3,2,1];
+delta_time = 5;
+t = [0,5,10,15,20,25];
+[discrete_encoder_count_rear_left, discrete_encoder_count_rear_right] = fcn_transform_encoderCounts(wheel_velocity_rear_left,wheel_velocity_rear_right,wheel_radius,initial_counts_rear_left,initial_counts_rear_right,delta_time,counts_per_revolution);
+plot(t,wheel_velocity_rear_left)
+hold on
+plot(t,discrete_encoder_count_rear_left)
+hold off
+legend("Velocities", "Rear Left Encoder Counts")
+
+%% Case:  
+wheel_velocity_rear_left = [1,1,2,2,3,3,4,4];
+wheel_velocity_rear_right = [6,5,4,3,2,1];
+delta_time = 1;
+t = [1,2,3,4,5,6,7,8];
+[discrete_encoder_count_rear_left, discrete_encoder_count_rear_right] = fcn_transform_encoderCounts(wheel_velocity_rear_left,wheel_velocity_rear_right,wheel_radius,initial_counts_rear_left,initial_counts_rear_right,delta_time,counts_per_revolution);
+plot(t,wheel_velocity_rear_left)
+hold on
+plot(t,discrete_encoder_count_rear_left)
+hold off
+legend("Velocities", "Rear Left Encoder Counts")
 
 %% Error throws
 if 1 == 0
